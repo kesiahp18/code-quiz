8f6010d4f630565eb8827477a599960b20833e49
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-11-08 22:06:42 -0500

    Cycle through questions and accept answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 var startButton = document.getElementById('start-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementsByClassName('answer-button')
+var answerButtonsElement = document.getElementById('answer-buttons')
 var firstButton = document.getElementById('button1')
+var secondButton = document.getElementById('button2')
+var thirdButton = document.getElementById('button3')
+var fourthButton = document.getElementById('button4')
+var feedbackElement = document.getElementById('feedback')
+
+var score = 0;
+
+var i = 0
 
 var questions = [
     {
@@ -13,22 +21,22 @@ var questions = [
             "option 3",
             "option 4"
         ],
-        answer: "answer 4"
+        answer: "option 4"
     },
     {
         question: 'Question 2 Here',
         choices: [
-            "option 1",
+            "option 1.1",
             "option 2",
             "option 3",
             "option 4"
         ],
-        answer: "answer 4"
+        answer: "option 1"
     },
     {
         question: 'Question 3 Here',
         choices: [
-            "option 1",
+            "option 1.2",
             "option 2",
             "option 3",
             "option 4"
@@ -38,17 +46,17 @@ var questions = [
     {
         question: 'Question 4 Here',
         choices: [
-            "option 1",
+            "option 1.3",
             "option 2",
             "option 3",
             "option 4"
         ],
-        answer: "answer 4"
+        answer: "option 4"
     },
     {
-        question: 'Question Here',
+        question: 'Question 5 Here',
         choices: [
-            "option 1",
+            "option 1.4",
             "option 2",
             "option 3",
             "option 4"
@@ -69,16 +77,49 @@ function startQuiz() {
 
 //Function to cycle through the questions
 function displayQuestions(question) {
-    //add the question text
-    questionElement.innerText = questions[0].question;
+    //debugger;
     //add the answer buttons text
-    debugger;
-    for (var i = 0; i < 3; i++); {
+    if(i < questions.length) {
+        //add the question text
+        questionElement.innerText = questions[i].question;
+        //add the button text
         document.getElementById("button1").innerText = questions[i].choices[0]
         document.getElementById("button2").innerText = questions[i].choices[1]
         document.getElementById("button3").innerText = questions[i].choices[2]
         document.getElementById("button4").innerText = questions[i].choices[3]
     }
-
+    else {
+        endQuiz();
+    }
 }
+
+function endQuiz() {
+    answerButtonsElement.classList.add('hide')
+    feedbackElement.classList.add('hide')
+    questionElement.innerText = "Your score is " + score
+
+    
+}
+
+function checkAnswer(e) {
+    var chosenAnswer = e.target
+    if (chosenAnswer.innerText === questions[i].answer) {
+        feedbackElement.classList.remove('hide')
+        feedbackElement.innerText = "correct"
+        score++;
+        i++;
+        displayQuestions()
+    } else {
+        feedbackElement.classList.remove('hide')
+        feedbackElement.innerText = "incorrect -5 seconds"
+        score--;
+        i++;
+        displayQuestions()
+    }
+}
+
 startButton.addEventListener('click', startQuiz)
+firstButton.addEventListener('click', checkAnswer)
+secondButton.addEventListener('click', checkAnswer)
+thirdButton.addEventListener('click', checkAnswer)
+fourthButton.addEventListener('click', checkAnswer)
